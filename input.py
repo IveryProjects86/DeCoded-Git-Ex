96e63447a965146a1e6d1d521c195a7e3d0de934
IveryProjects86 <Atirasdotcom@me.com>
2020-05-28 15:52:52 -0500

    Changed the right variable

--- input.py
+++ input.py
@@ -1,4 +1,4 @@
 # Number 2 is telling the computer to output my input, run the code to see it displayed
-age2 = input("Please enter your age: ")
-print(age2)
+age7 = input("Please enter your age: ")
+print(age7)
 # 3 is printing out the value, type age and then press enter
