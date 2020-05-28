7f1d6b43918f63a10189306ef07f3317234d6a65
IveryProject86 <Atirasdotcom@me.com>
2020-05-28 14:30:15 -0500

    Added program to display output

---
+++ input.py
@@ -0,0 +1,4 @@
+# Number 2 is telling the computer to output my input, run the code to see it displayed
+age = input("Please enter your age: ")
+print(age)
+# 3 is printing out the value, type age and then press enter
